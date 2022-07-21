5adcc99c079e27dc49f693e7a3e7f8ecaef4f994
hubo <hubo@gohigh.com.cn>
2022-07-21 14:39:26 +0800

    fix ft script bug

--- tcpio/asio/ft/build_ft.sh
+++ tcpio/asio/ft/build_ft.sh
@@ -1,17 +1,18 @@
 reset; clear;
-cur_dir=$(cd "$(dirname "$0")"; pwd)
-echo "current dir path is: ${cur_dir}"
-rm -rf ${cur_dir}/build
+SCRIPT_PATH=`realpath $0`
+SCRIPT_DIR=`dirname $SCRIPT_PATH`
+rm -rf ${SCRIPT_DIR}/build
+cd ${SCRIPT_DIR}
 mkdir build
 cd build
-cmake ..  -DFT_TEST=1
+cmake ${SCRIPT_DIR}  -DFT_TEST=1
 make -j2
 ./ft
 
 ################## for lcov ##################
 mkdir coverage_result
 cd coverage_result
-lcov --rc lcov_branch_coverage=1 -q -c -d ${cur_dir}/build -o app_cov.info
+lcov --rc lcov_branch_coverage=1 -q -c -d ${SCRIPT_DIR}/build -o app_cov.info
 lcov --rc lcov_branch_coverage=1 -q -e app_cov.info "*/app/*" -o app_cov.info
 #lcov --rc lcov_branch_coverage=1 -q -e app_cov.info "*.cpp" -o app_cov.info
 genhtml -q --rc lcov_branch_coverage=1 app_cov.info
