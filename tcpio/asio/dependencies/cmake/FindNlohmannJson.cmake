f9965660a816feb5131c5f892428d9fbd38f75f9
hubo <hubo@cictci.com>
2022-12-20 17:20:21 +0800

    Revert "add dependency"
    This reverts commit ebb31332db3037f42ad9f2411327ba5e1194284b.

--- tcpio/asio/dependencies/cmake/FindNlohmannJson.cmake
+++
@@ -1,5 +0,0 @@
-cmake_minimum_required(VERSION 3.5)
-
-set(NLOHMANNJSON_INCLUDES ${PROJECT_SOURCE_DIR}/dependencies/nlohmann)
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NlohmannJson DEFAULT_MSG NLOHMANNJSON_INCLUDES)
